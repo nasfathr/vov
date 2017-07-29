67dcb8de6a4fb9398c855330b19db45b523a05ea
Adly Abdullah <adly@trustedcompany.com>
2017-07-29 17:45:12 +0800

    Implement fake login

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var hbs = require('express-hbs');
-
+var lowdb = require('lowdb');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+ 
 var site = express();
 
 site.engine('hbs', hbs.express4({
@@ -13,9 +16,32 @@ site.get('/favicon.*',function(req,res){
 	res.send('');
 });
 
-site.get('/',function(req,res){
+site.use(session({
+	secret: '0o1q9i2w8u3e7y4r6t5',
+	resave: false,
+	store: new FileStore({}),
+	saveUninitialized: true,
+	cookie: {}
+}));
+
+site.get('/', (req,res) => {
 	res.render('index.hbs',{
-		welcome_message: 'Hello World!!'
+		welcome_message: 'Hello World!!',
+		session: req.session
+	});
+});
+
+site.post('/register', (req,res) => {
+	req.session.loggedIn = 1;
+});
+
+site.post('/login', (req,res) => {
+	req.session.loggedIn = 1;
+});
+
+site.get('/logout', (req,res) => {
+	req.session.destroy(()=>{
+		res.redirect('/');
 	});
 });
 
